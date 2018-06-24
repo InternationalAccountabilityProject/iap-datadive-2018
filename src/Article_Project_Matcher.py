7a51c7c823204d60bba0d662977f60f61cc03f1a
jchow0591 <jchow0591@gmail.com>
2018-06-23 20:09:58 -0400

    message

--- src/Article_Project_Matcher.py
+++ src/Article_Project_Matcher.py
@@ -2,10 +2,12 @@ import numpy as np
 from sklearn.externals import joblib
 import spacy
 from gensim.models import KeyedVectors
+from sklearn.metrics.pairwise import euclidean_distances
 import pandas as pd
+nlp = spacy.load('en')
 
-W2V_EMBEDDINGS = "wiki-news-300d-1M.vec"
-PROJECT_EMBEDDINGS = "project.csv"
+W2V_EMBEDDINGS = "dependencies/wiki-news-300d-1M.vec"
+PROJECT_EMBEDDINGS = "data/EWS_Published Project_Listing_DD.csv"
 STOP_WORDS = "stopwords.csv"
 
 class ArticleProjectMatcher(object):
@@ -15,41 +17,39 @@ class ArticleProjectMatcher(object):
     """
     def __init__(self, project_ids, project_embeddings, w2v, stopwords):
         self.project_ids = project_ids
-        self.project_embeddings = project_embeddings
         self.w2v = KeyedVectors.load_word2vec_format(w2v)
-        self.stopwords = stopwords.split(",")
+        with open(stopwords, 'r') as f:
+            self.stopwords = set(f.read().strip().split("\n"))
+        project_df = pd.read_csv(project_embeddings, encoding='ISO-8859-1')
+        self.project_embeddings = {x[0]: x[1] for x in project_df[['Project ID', 'fasttext_embedding']].values}
 
-    def add_project_to_list(project, project_idx):
-        """
-        Append a new project id and embedding to our object's memory.
-        """
-        self.project_ids.append(project_idx)
-        embed_proj = self.create_embedding(project)
-        self.project_embeddings.append(embed_proj)
-
-    def create_embedding(textbody):
+    def create_embedding(self, textbody):
         """
         Creates a document embedding of some text body by averaging the word
         embeddings of all words in the text body.
         """
         embedding = []
+        textbody = nlp(textbody)
         for tok in textbody:
             if tok not in self.stopwords and tok.text in self.w2v:
-                embedding.append(w2v[tok.text])
-        return np.mean(embedding, axis = 0) if len(embedding) > 0 else None
+                embedding.append(self.w2v[tok.text])
+        return np.mean(embedding, axis=0) if len(embedding) > 0 else None
 
 
-    def compute_similarity(article):
+    def compute_similarity(self, article):
         """
         Given an article body, compute the semantic similarity between the
         article and all project embeddings stored in the class memory.
         """
-        match_score = []
+        match_scores = []
         article_embedding = create_embedding(article)
-        for idx, project in enumerate(self.project_embeddings):
-            if project is not None:
-                simlarity_score = np.linalg.norm(project - article_embedding)
-                match_scores.append((self.project_ids[idx], similarity_score))
+        if article_embedding is None:
+            return match_scores
+        for project in self.project_embeddings:
+            if self.project_embeddings[project] is not None:
+                sim_score = euclidean_distances(article_embedding.reshape(1, -1)
+                                                , self.project_embeddings[project].reshape(1, -1))[0][0]
+                match_scores.append((project, sim_score))
             else:
-                match_scores.append((self.project_ids[idx], 0.0))
+                match_scores.append((project, None))
         return match_scores
